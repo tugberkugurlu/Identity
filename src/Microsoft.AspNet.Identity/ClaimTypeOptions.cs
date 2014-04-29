16efc5e22ed0afa7a96f9efc0d0297916fe1d3ea
Hao Kung <haok@microsoft.com>
2014-04-29 13:47:08 -0700

    Split options into their own classes

---
+++ src/Microsoft.AspNet.Identity/ClaimTypeOptions.cs
@@ -0,0 +1,53 @@
+using System.Security.Claims;
+
+namespace Microsoft.AspNet.Identity
+{
+    public class ClaimTypeOptions
+    {
+        /// <summary>
+        ///     ClaimType used for the security stamp by default
+        /// </summary>
+        public static readonly string DefaultSecurityStampClaimType = "AspNet.Identity.SecurityStamp";
+
+        public ClaimTypeOptions()
+        {
+            Role = ClaimTypes.Role;
+            SecurityStamp = DefaultSecurityStampClaimType;
+            UserId = ClaimTypes.NameIdentifier;
+            UserName = ClaimTypes.Name;
+            
+        }
+
+        /// <summary>
+        ///     Claim type used for role claims
+        /// </summary>
+        public string Role { get; set; }
+
+        /// <summary>
+        ///     Claim type used for the user name
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        ///     Claim type used for the user id
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        ///     Claim type used for the user security stamp
+        /// </summary>
+        public string SecurityStamp { get; set; }
+
+        public virtual void Copy(ClaimTypeOptions options)
+        {
+            if (options == null)
+            {
+                return;
+            }
+            Role = options.Role;
+            SecurityStamp = options.SecurityStamp;
+            UserId = options.UserId;
+            UserName = options.UserName;
+        }
+    }
+}
